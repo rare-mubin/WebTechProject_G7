e9bcdf96183d2ed186217b36117ddf5f318a6072
Tasnuba-Tabassum <tasnubatabassum593@gmail.com>
2026-05-13 22:00:29 +0600

    Add sign-up form html

---
+++ View/signUp.php
@@ -0,0 +1,70 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sign Up Page</title>
+    <link rel="stylesheet" href="assets/css/signup.css">
+</head>
+
+<body>
+    <div class="signUpPage">
+        <div class="signUpWrapper">
+            <div class="leftPart">
+                <form class="signUpForm" action="" method="post">
+
+                    <h1 class="pageTitle">Sign Up</h1>
+                    <p class="pageSubtitle">Create account</p>
+
+                    <div class="fieldGroup">
+                        <label for="name" class="formLabel">Name</label>
+                        <input type="text" id="name" name="name" class="formControl" required>
+                    </div>
+
+                    <div class="fieldGroup">
+                        <label for="email" class="formLabel">Email</label>
+                        <input type="email" id="email" name="email" class="formControl" required>
+                    </div>
+
+                    <div class="fieldGroup">
+                        <label for="phone" class="formLabel">Phone</label>
+                        <input type="text" id="phone" name="phone" class="formControl"required>
+                    </div>
+
+                    <div class="fieldGroup">
+                        <label for="nationality" class="formLabel">Nationality</label>
+                        <div class="selectionWrap">
+                            <select id="nationality" name="nationality" class="formSelection" required>
+                                <option value="">Select nationality</option>
+                                <option value="Bangladeshi">Bangladeshi</option>
+                                <option value="Indian">Indian</option>
+                                <option value="Pakistani">Pakistani</option>
+                                <option value="Nepali">Nepali</option>
+                                <option value="Sri Lankan">Sri Lankan</option>
+                                <option value="American">American</option>
+                                <option value="Other">Other</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="fieldGroup">
+                        <label for="password" class="formLabel">Password</label>
+                        <input type="password" id="password" name="password" class="formControl" required>
+                    </div>
+
+                    <button type="submit" class="submitButton">Create Account</button>
+
+                    <p class="loginLink"> Already have an account? <a href="login.php">Log Up</a></p>
+                </form>
+            </div>
+
+            <div class="rightPart">
+                <div class="hotelRightImage">
+                    <img src="assets/images/login-side-image.jpg" alt="Hotel View">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
