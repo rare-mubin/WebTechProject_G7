9fb290fc1eccc295ed283f72bd3951256423edb5
rare-mubin <mvrdark@gmail.com>
2026-05-14 03:02:17 +0600

    Add database connection class with methods for connection and record retrieval

---
+++ Model/dataConnection.php
@@ -0,0 +1,26 @@
+<?php
+    class db
+    {
+        function connection()
+        {
+            $db_host = "localhost";
+            $db_user= "root";
+            $db_password="";
+            $db_name="hotel_r";
+
+            $connection=  new mysqli($db_host, $db_user,$db_password,$db_name);
+            if($connection->connect_error)
+            {
+                die ("Could not Connect Database".$connection->connect_error);
+            }
+            return $connection;
+        }
+
+        function getRecords($connection, $tablename)
+        {   
+            $sql = "SELECT * FROM $tablename";
+            $result = $connection->query($sql);
+            return $result;
+        }
+    }
+?>
